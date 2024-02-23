757a18482a508d20352c3a0513e216ba255823ae
DoraStark <156120925+DoraStark@users.noreply.github.com>
2024-02-23 07:38:53 +0100

    MatrixTest.java
    1 commit

--- MatrixTest.java
+++ MatrixTest.java
@@ -1,46 +1,42 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class MatrixTest {
+public class MatrixTest {
 
+    private Matrix matrix;
+
+    @BeforeEach
+    public void setUp() {
+        matrix = new Matrix();
+    }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
+        matrix = null;
     }
 
     @Test
     void avgArray() {
-
-            Matrix matrix = new Matrix();
-            int[] testArray = {1, 2, 3, 4, 5};
-            double expectedAverage = (1 + 2 + 3 + 4 + 5) / (double) testArray.length;
-            assertEquals(expectedAverage, matrix.avgArray(testArray), 0.001);
-        }
-
+        int[] testArray = {1, 2, 3, 4, 5};
+        double expectedAverage = (1 + 2 + 3 + 4 + 5) / (double) testArray.length;
+        assertEquals(expectedAverage, matrix.avgArray(testArray), 0.001);
+    }
 
     @Test
     void isMatrixSquare() {
-
-        Matrix matrix = new Matrix();
-
-
         int[][] nullMatrix = null;
         assertFalse(matrix.isMatrixSquare(nullMatrix));
-
 
         int[][] emptyMatrix = {};
         assertFalse(matrix.isMatrixSquare(emptyMatrix));
 
-
         int[][] nonSquareMatrix = {
-                {1, 2, 3, 8 },
+                {1, 2, 3, 8},
                 {4, 5, 6}
         };
         assertFalse(matrix.isMatrixSquare(nonSquareMatrix));
-
 
         int[][] squareMatrix = {
                 {1, 2, 3},
@@ -49,4 +45,4 @@ class MatrixTest {
         };
         assertTrue(matrix.isMatrixSquare(squareMatrix));
     }
-    }
+}
